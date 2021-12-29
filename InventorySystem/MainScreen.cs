50a8edfb5de2c1dd69544d3f049333e7ffe5901f
tgoodf1 <tyler.goodfred@gmfinancial.com>
2021-12-29 14:55:02 -0600

    added product objects, and new main screen forms

--- InventorySystem/MainScreen.cs
+++ InventorySystem/MainScreen.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Documents;
 using System.Windows.Forms;
 
 namespace InventorySystem
@@ -15,6 +17,12 @@ namespace InventorySystem
         public MainScreen()
         {
             InitializeComponent();
+            Product test1 = new Product();
+            test1.Name = "Card";
+            test1.Price = 4.99;
+            test1.StockNum = 678;
+
+            Console.WriteLine("total cost is: " + Product.GetTotalCost(test1.Price, test1.StockNum));
         }
     }
 }
---
+++ InventorySystem/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventorySystem
+{
+    internal class Product
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public int ItemSKU { get; set; }
+        public int StockNum { get; set; }
+
+        public static double GetTotalCost(double price, int stock)
+        {
+            return price * stock;
+        }
+    }
+}
