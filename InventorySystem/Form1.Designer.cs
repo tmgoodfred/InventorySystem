d8c2e3c08c04d2febf4f0de7045c8d4b36b51bbe
tgoodf1 <tyler.goodfred@gmfinancial.com>
2021-12-29 11:27:27 -0600

    Add project files.

---
+++ InventorySystem/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace InventorySystem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.usernameTxtBx = new System.Windows.Forms.TextBox();
+            this.passwordTxtBx = new System.Windows.Forms.MaskedTextBox();
+            this.loginSubmitBtn = new System.Windows.Forms.Button();
+            this.errorLbl = new System.Windows.Forms.Label();
+            this.usernameLbl = new System.Windows.Forms.Label();
+            this.passwordLbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // usernameTxtBx
+            // 
+            this.usernameTxtBx.Location = new System.Drawing.Point(132, 50);
+            this.usernameTxtBx.Name = "usernameTxtBx";
+            this.usernameTxtBx.Size = new System.Drawing.Size(100, 20);
+            this.usernameTxtBx.TabIndex = 0;
+            // 
+            // passwordTxtBx
+            // 
+            this.passwordTxtBx.Location = new System.Drawing.Point(132, 85);
+            this.passwordTxtBx.Name = "passwordTxtBx";
+            this.passwordTxtBx.Size = new System.Drawing.Size(100, 20);
+            this.passwordTxtBx.TabIndex = 1;
+            // 
+            // loginSubmitBtn
+            // 
+            this.loginSubmitBtn.Location = new System.Drawing.Point(118, 127);
+            this.loginSubmitBtn.Name = "loginSubmitBtn";
+            this.loginSubmitBtn.Size = new System.Drawing.Size(75, 23);
+            this.loginSubmitBtn.TabIndex = 2;
+            this.loginSubmitBtn.Text = "Login";
+            this.loginSubmitBtn.UseVisualStyleBackColor = true;
+            this.loginSubmitBtn.Click += new System.EventHandler(this.loginSubmitBtn_Click);
+            // 
+            // errorLbl
+            // 
+            this.errorLbl.AutoSize = true;
+            this.errorLbl.Location = new System.Drawing.Point(165, 165);
+            this.errorLbl.Name = "errorLbl";
+            this.errorLbl.Size = new System.Drawing.Size(0, 13);
+            this.errorLbl.TabIndex = 3;
+            // 
+            // usernameLbl
+            // 
+            this.usernameLbl.AutoSize = true;
+            this.usernameLbl.Location = new System.Drawing.Point(71, 53);
+            this.usernameLbl.Name = "usernameLbl";
+            this.usernameLbl.Size = new System.Drawing.Size(55, 13);
+            this.usernameLbl.TabIndex = 4;
+            this.usernameLbl.Text = "Username";
+            // 
+            // passwordLbl
+            // 
+            this.passwordLbl.AutoSize = true;
+            this.passwordLbl.Location = new System.Drawing.Point(73, 88);
+            this.passwordLbl.Name = "passwordLbl";
+            this.passwordLbl.Size = new System.Drawing.Size(53, 13);
+            this.passwordLbl.TabIndex = 5;
+            this.passwordLbl.Text = "Password";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(327, 226);
+            this.Controls.Add(this.passwordLbl);
+            this.Controls.Add(this.usernameLbl);
+            this.Controls.Add(this.errorLbl);
+            this.Controls.Add(this.loginSubmitBtn);
+            this.Controls.Add(this.passwordTxtBx);
+            this.Controls.Add(this.usernameTxtBx);
+            this.Name = "Form1";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox usernameTxtBx;
+        private System.Windows.Forms.MaskedTextBox passwordTxtBx;
+        private System.Windows.Forms.Button loginSubmitBtn;
+        private System.Windows.Forms.Label errorLbl;
+        private System.Windows.Forms.Label usernameLbl;
+        private System.Windows.Forms.Label passwordLbl;
+    }
+}
+
